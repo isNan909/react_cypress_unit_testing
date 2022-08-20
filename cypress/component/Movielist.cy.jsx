904c34b03f73aae069b0a031c7190cf46ba65855
ishan-me <ishan02016@gmail.com>
2022-08-20 21:00:27 +0545

    added some test

--- cypress/component/Movielist.cy.jsx
+++ cypress/component/Movielist.cy.jsx
@@ -4,34 +4,38 @@ import '../../src/App.css'
 describe('<Movielist>', () => {
   beforeEach(() => {
     cy.mount(<Movielist />);
-  });
-  it('Mounts a component and write', () => {
     cy.get('[data-cy=empty]').contains('No movies here');
-    cy.get('form input').should('have.value', '');
-
-  })
-  it('The Listof movies appends', () => {
-    cy.get('form input')
-      .type('Snowden')
-      .should('have.value', 'Snowden')
+    const formInput = cy.get('form input');
+    formInput.should('have.value', '');
+    formInput.type('Monster Inc.')
+      .should('have.value', 'Monster Inc.')
     cy.get('form button').click();
-    cy.get('[data-cy=movie-list]').children().should('have.length', 1);
+    formInput.clear()
+    formInput.type('Circle of eight')
+      .should('have.value', 'Circle of eight');
+    cy.get('form button').click();
+    cy.get('[data-cy=movie-list]').children().should('have.length', 2);
+  });
+  // it('Mounts a component and write', () => {
+  //   cy.get('[data-cy=empty]').contains('No movies here');
+  //   cy.get('form input').should('have.value', '');
+
+  // })
+  it('The Listof movies appends', () => {
     cy.get('form input')
       .type('Monster Inc.')
       .should('have.value', 'Monster Inc.')
     cy.get('form button').click();
-    cy.get('[data-cy=movie-list]').children().should('have.length', 2);
+    cy.get('[data-cy=movie-list]').children().should('have.length', 3);
   })
+
   it('uncheck movie', () => {
-    cy.get('form input')
-      .type('The Machinist')
-      .should('have.value', 'The Machinist')
-    cy.get('form button').click();
+    const lastListitem = cy.get('[data-cy=movie-list]:nth-child(1) li:last-child');
+    lastListitem.click();
+    lastListitem.should('have.class', 'strike');
+    cy.get('[data-cy=clear-movie]').click();
     cy.get('[data-cy=movie-list]').children().should('have.length', 1);
-    cy.get('form input')
-      .type('Circle of eight')
-      .should('have.value', 'Circle of eight')
-    cy.get('form button').click();
-    cy.get('[data-cy=movie-list]').children().should('have.length', 2);
+    cy.get('[data-cy=clear-movie]').click();
+    cy.get('[data-cy=movie-list]').children().should('have.length', 1);
   })
 })
