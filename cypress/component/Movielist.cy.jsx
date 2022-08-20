774e9cc6441194f932ffc2680fd64efe853ba59e
ishan-me <ishan02016@gmail.com>
2022-08-20 17:39:59 +0545

    added some test

--- cypress/component/Movielist.cy.jsx
+++ cypress/component/Movielist.cy.jsx
@@ -2,7 +2,36 @@ import Movielist from '../../src/components/Movielist';
 import '../../src/App.css'
 
 describe('<Movielist>', () => {
-  it('mounts', () => {
-    cy.mount(<Movielist />)
+  beforeEach(() => {
+    cy.mount(<Movielist />);
+  });
+  it('Mounts a component and write', () => {
+    cy.get('[data-cy=empty]').contains('No movies here');
+    cy.get('form input').should('have.value', '');
+
+  })
+  it('The Listof movies appends', () => {
+    cy.get('form input')
+      .type('Snowden')
+      .should('have.value', 'Snowden')
+    cy.get('form button').click();
+    cy.get('[data-cy=movie-list]').children().should('have.length', 1);
+    cy.get('form input')
+      .type('Monster Inc.')
+      .should('have.value', 'Monster Inc.')
+    cy.get('form button').click();
+    cy.get('[data-cy=movie-list]').children().should('have.length', 2);
+  })
+  it('uncheck movie', () => {
+    cy.get('form input')
+      .type('The Machinist')
+      .should('have.value', 'The Machinist')
+    cy.get('form button').click();
+    cy.get('[data-cy=movie-list]').children().should('have.length', 1);
+    cy.get('form input')
+      .type('Circle of eight')
+      .should('have.value', 'Circle of eight')
+    cy.get('form button').click();
+    cy.get('[data-cy=movie-list]').children().should('have.length', 2);
   })
 })
--- src/components/Movielist.jsx
+++ src/components/Movielist.jsx
@@ -21,7 +21,6 @@ function Movielist() {
 			return m.id === Number(id) ? { ...m, seen: !m.seen } : { ...m };
 		});
 		setMovieList(markSeen);
-		console.log(movieList);
 	}
 
 	const handleSeen = () => {
@@ -38,16 +37,16 @@ function Movielist() {
 				<button>+Add movies</button>
 			</form>
 			<div className="movieList">
-				{!movieList && <span class="empty">No movies here</span>}
-				<ul>
+				{!movieList && <span data-cy='empty' className="empty">No movies here</span>}
+				<ul data-cy='movie-list'>
 					{movieList && movieList.map(m => {
 						return (
 							<li onClick={handleClick} className={m.seen ? "strike movie-list" : "movie-list"} id={m.id} key={m.id}>
 								<span>
 									{m.movie}
-									<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+									<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
 										<path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-										<path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
+										<path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
 									</svg>
 								</span>
 							</li>
@@ -55,7 +54,7 @@ function Movielist() {
 					})}
 				</ul>
 			</div>
-			{movieList.length > 0 && <><button className="outline-btn" onClick={handleSeen}>Clear seen movies</button></>}
+			{movieList.length > 0 && <><button data-cy='clear-movie' className="outline-btn" onClick={handleSeen}>Clear seen movies</button></>}
 		</div>
 	)
 }
